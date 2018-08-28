8081fe78d33b26e460bdf24156777e1fb8fe9924
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-27 22:47:41 -0300

    Change DB to Mongo

--- app/Controllers/Http/GamesController.js
+++ app/Controllers/Http/GamesController.js
@@ -1,18 +1,52 @@
 'use strict'
 
-const {igdbApi} = require('../../../config/services')
+const { igdbApi } = require('../../../config/services')
+const moment = require('moment')
+const translate = require('google-translate-api')
+const Game = use('App/Models/Game')
 
 class GamesController {
-
   /**
    * Show a list of games.
    * GET games
    */
   async index ({ request, params }) {
-    //const { page } = request.get()
+    // const { page } = request.get()
     let query = params.query
 
-    const { data: games } = await igdbApi.get(`/games/?search=${query}&fields=*`);
+    const { data: games } = await igdbApi.get(
+      `/games/?search=${query}&fields=*`
+    )
+
+    games.forEach(async g => {
+      try {
+        const gameDB = await Game.where('id')
+          .eq(g.id)
+          .fetch()
+
+        if (
+          !gameDB.first() ||
+          moment().diff(
+            moment(gameDB.first().translated_at, 'YYYY-MM-DD'),
+            'days'
+          ) > 7
+        ) {
+          const { text: translated } = await translate(g.summary, {
+            from: 'en',
+            to: 'pt'
+          })
+          g.translated_summary = translated
+          g.translated_at = moment().format('YYYY-MM-DD')
+
+          let game = new Game()
+          game = g
+
+          await Game.create(game)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    })
 
     return games
   }
--- config/auth.js
+++ config/auth.js
@@ -26,52 +26,42 @@ module.exports = {
   | Session authentication is always persistent.
   |
   */
-  session: {
-    serializer: 'lucid',
-    model: 'App/Models/User',
-    scheme: 'session',
-    uid: 'email',
-    password: 'password'
-  },
+ session: {
+  serializer: 'LucidMongo',
+  model: 'App/Models/User',
+  scheme: 'session',
+  uid: 'email',
+  password: 'password'
+},
 
-  /*
-  |--------------------------------------------------------------------------
-  | Basic Auth
-  |--------------------------------------------------------------------------
-  |
-  | The basic auth authenticator uses basic auth header to authenticate a
-  | user.
-  |
-  | NOTE:
-  | This scheme is not persistent and users are supposed to pass
-  | login credentials on each request.
-  |
-  */
-  basic: {
-    serializer: 'lucid',
-    model: 'App/Models/User',
-    scheme: 'basic',
-    uid: 'email',
-    password: 'password'
-  },
+basic: {
+  serializer: 'LucidMongo',
+  model: 'App/Models/User',
+  scheme: 'basic',
+  uid: 'email',
+  password: 'password'
+},
 
-  /*
-  |--------------------------------------------------------------------------
-  | Jwt
-  |--------------------------------------------------------------------------
-  |
-  | The jwt authenticator works by passing a jwt token on each HTTP request
-  | via HTTP `Authorization` header.
-  |
-  */
-  jwt: {
-    serializer: 'lucid',
-    model: 'App/Models/User',
-    scheme: 'jwt',
-    uid: 'email',
-    password: 'password',
-    options: {
-      secret: Env.get('APP_KEY')
-    }
+jwt: {
+  serializer: 'LucidMongo',
+  model: 'App/Models/User',
+  token: 'App/Models/Token',
+  scheme: 'jwt',
+  uid: 'email',
+  password: 'password',
+  expiry: '20m',
+  options: {
+    secret: 'self::app.appKey'
   }
+},
+
+api: {
+  serializer: 'LucidMongo',
+  scheme: 'api',
+  model: 'App/Models/User',
+  token: 'App/Models/Token',
+  uid: 'username',
+  password: '',
+  expiry: '30d',
+}
 }
